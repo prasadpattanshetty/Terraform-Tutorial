1beff9f309a405feed5ba2c3869446676d882a16
prasadpattanshetty <78920753+prasadpattanshetty@users.noreply.github.com>
2021-07-03 18:32:46 +0530

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,13 +1,13 @@
 variable "region" {
-  default = "ap-south-1"
+  default = "ap-south-1a"
 }
 
 variable "ami_id" {
   type = "map"
 
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
+    ap-south-1a   = "ami-035b3c7efe6d061d5"
+    ap-south-1b    = "ami-132b3c7efe6sdfdsfd"
+    ap-south-1c = "ami-9787h5h6nsn"
   }
 }
